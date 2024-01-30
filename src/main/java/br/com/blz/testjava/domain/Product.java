e02e542faed54c3ad261e85d280a8c5f7868d750
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2024-01-29 23:44:45 -0300

    feat(main): criação do product dentro do domain que contém o sku, nome e a chamada pro objeto inventory

---
+++ src/main/java/br/com/blz/testjava/domain/Product.java
@@ -0,0 +1,42 @@
+package br.com.blz.testjava.domain;
+
+public class Product {
+
+    private int sku;
+    private String name;
+    private Inventory inventory;
+
+    boolean isMarketable;
+
+    public int getSku() {
+        return sku;
+    }
+
+    public void setSku(int sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public boolean isMarketable() {
+        return isMarketable;
+    }
+
+    public void setMarketable(boolean marketable) {
+        isMarketable = marketable;
+    }
+}
