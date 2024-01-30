e77b784baa445d85fe4c1fb912a7d804c9bd6b08
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2024-01-29 23:50:03 -0300

    feat(main): criação do controller referente as funcionalidades para os produtos.

---
+++ src/main/java/br/com/blz/testjava/controller/ProductController.java
@@ -0,0 +1,49 @@
+package br.com.blz.testjava.controller;
+
+import br.com.blz.testjava.domain.Product;
+import br.com.blz.testjava.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{sku}")
+    public ResponseEntity<Void> updateProduct(@RequestBody Product product) {
+        productService.updateProduct(product, product.getSku());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{sku}")
+    public ResponseEntity<Product> getProductBySku(@PathVariable int sku) {
+        Optional<Product> product = productService.getProductBySku(sku);
+
+        return product.map(ResponseEntity::ok)
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+
+    }
+
+    @DeleteMapping("/{sku}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int sku) {
+        productService.deleteProduct(sku);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
