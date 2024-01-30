1a57228bc270f67072be45665d73fad8c5432225
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2024-01-29 23:49:34 -0300

    feat(main): criação do service com as implementações.

---
+++ src/main/java/br/com/blz/testjava/service/ProductService.java
@@ -0,0 +1,57 @@
+package br.com.blz.testjava.service;
+
+import br.com.blz.testjava.domain.Inventory;
+import br.com.blz.testjava.domain.Product;
+import br.com.blz.testjava.domain.Warehouse;
+import br.com.blz.testjava.exception.ProductAlreadyExistsException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public Product createProduct(Product product) {
+        if (getProductBySku(product.getSku()).isPresent()) {
+            throw new ProductAlreadyExistsException("Já existe produto cadastrado com essa sku");
+        }
+        products.add(product);
+
+        return product;
+    }
+
+    public void updateProduct(Product product, int sku) {
+        getProductBySku(sku).ifPresent(productFound -> {
+            productFound.setInventory(product.getInventory());
+            productFound.setName(product.getName());
+        });
+    }
+
+    public Optional<Product> getProductBySku(int sku) {
+        return products.stream()
+            .filter(product -> product.getSku() == sku)
+            .peek(this::calculateInventoryQuantity)
+            .peek(this::checkIfProductIsMarketable)
+            .findFirst();
+    }
+
+    public void deleteProduct(int sku) {
+        getProductBySku(sku).ifPresent(products::remove);
+    }
+
+    private void checkIfProductIsMarketable(Product product) {
+        product.setMarketable(product.getInventory().getQuantity() > 0);
+    }
+
+    private void calculateInventoryQuantity(Product product) {
+        Inventory inventory = product.getInventory();
+        int totalQuantity = inventory.getWarehouses().stream()
+            .mapToInt(Warehouse::getQuantity)
+            .sum();
+        inventory.setQuantity(totalQuantity);
+    }
+}
