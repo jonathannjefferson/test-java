c947c9d7c044c85247b127dd0d1eb8def583ec93
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2024-01-30 23:26:02 -0300

    refactor(main): ajuste dos imports

--- src/main/java/br/com/blz/testjava/controller/ProductController.java
+++ src/main/java/br/com/blz/testjava/controller/ProductController.java
@@ -4,13 +4,22 @@ import br.com.blz.testjava.domain.Product;
 import br.com.blz.testjava.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
-public class ProductController {
+public class
+ProductController {
 
     private final ProductService productService;
 
