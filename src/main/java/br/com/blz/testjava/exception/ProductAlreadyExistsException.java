197806f71c71de834fce3edff01f4fe99e6d61d8
Jonathann Jefferson <jonathannjefferson10@gmail.com>
2024-01-30 22:14:11 -0300

    remove(main): remoção de classe não utilizada

--- src/main/java/br/com/blz/testjava/exception/ProductAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package br.com.blz.testjava.exception;
-
-public class ProductAlreadyExistsException extends RuntimeException{
-
-    public ProductAlreadyExistsException(String message) {
-        super(message);
-    }
-}
